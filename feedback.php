2d9616745861048502323dc73f5dd4821fe24ebf
Monil Shah <shahmonil1996@gmail.com>
2016-01-17 19:46:58 +0530

    First Commit

---
+++ feedback.php
@@ -0,0 +1,102 @@
+
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- css files start-->
+<link rel="stylesheet" type="text/css" href="css/component.css" /><!-- Blue Modal -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/common.css" />
+<!-- Custom Theme files -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<link rel="stylesheet" type="text/css" href="css/style3.css" />
+<!-- javascript files end-->
+</head>
+	</head>
+	<body>
+	<!-- navbar start-->
+	<div class="navbar">
+<div class="logo">
+	<h2><a href="#"><img src="images/xBITS_Logo_transparent.png" style="height:50px;width:100px;"></h2>
+</div>
+<div class="top-navg" >
+			 	    <span class="menu"> <img src="images/icon.png" alt=""/></span>
+			 	<ul class="res">
+			 		<li><a class="scroll" href="#home">Home</a></li>
+			 		<li><a class="scroll" href="#about">About</a></li>		 		
+			 		<li><a class="scroll" href="#product">Product &#9662;</a>
+			 		<ul class="dropdown">
+				 		<li><a href="rightbiotic.html">Right<span style="color:red">B</span>iotic</a></li>
+	                	
+			 		</ul></li>
+			 		<li><a class="scroll" href="#team">Team</a></li>
+                                        <li><a class="scroll" href="#contact">Contact &#9662;</a>
+                                        <ul class="dropdown">
+				 		<li><a href="feedback.php">Feedback</a></li>
+	                	
+			 		</ul></li>
+			 		<li><a  href="news.html">News</a></li>
+                                        <li><a  href="awards.html">Awards</a></li>
+			 		
+			 		
+			 	</ul>
+			 	<!-- script-for-menu -->
+						 <script>
+						   $( "span.menu" ).click(function() {
+							 $( "ul.res" ).slideToggle( 300, function() {
+							 // Animation complete.
+							  });
+							 });
+						</script>
+			 </div>
+			<div class="clearfix"> </div>
+</div>
+</div>
+<!-- navbar end -->
+<div class="banned"> </div>
+
+<!--map end here-->
+<!--contact start here-->
+<div class="contact" id="contact">
+	<div class="container">
+		<div class="contact-main">
+			<div class="contact-top">
+			   <h3>Contact us</h3>
+				
+			</div>
+			<div class="contact-bott">
+				<input type="text" value="Name"  onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}">
+				<input type="text" value="Email" class="email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'email';}">
+				<input type="text" value="phone" class="name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'phone';}">
+				<textarea onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Message';}">Message</textarea>
+				<input type="submit" value="Send">
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+</div>
+<!--contact end here-->
+<!--footer start here-->
+<div class="footer">
+
+	
+	<div class="col-lg-10">
+	<p style="color:black;">Copyright &copy; 2016 Right<span style="color:red">B</span>iotic &nbsp;&nbsp;&nbsp;All Rights Reserved</p>
+	</div>
+	<div class="col-lg-1">
+	<div class="col-lg-6">
+	<a href="www.facebook.com/rightbiotic" style="color:black">
+	</div>
+	<div class="col-lg-6">
+	<img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xpa1/v/t1.0-1/p50x50/1382062_574357299267862_1997202362_n.png?oh=abfa678d9ff621c530ab0cbac8e8a2f9&oe=57413CB2&__gda__=1463092764_dc7f10ebd219789b0316199a89559d89">RightBiotic</a>
+
+	</div>
+</div>
+<!--footer end here-->
+
+    
+	</body>
+</html>
